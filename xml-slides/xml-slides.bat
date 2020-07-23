4d19a54d14f90eec3b2d1b96e11a387a2389b7a6
Jon Gunderson <jongund@illinois.edu>
2020-07-22 20:43:34 -0500

    examples

--- xml-slides/xml-slides.bat
+++ xml-slides/xml-slides.bat
@@ -1,4 +1,4 @@
-set SLIDEPATH=C:\inetpub\wwwroot\presentations\xml-slides
+set SLIDEPATH=C:\inetpub\wwwroot\xml-slides\xml-slides
 set SLIDECLASSPATH=%SLIDEPATH%\xalan\serializer.jar
 set SLIDECLASSPATH=%SLIDECLASSPATH%;%SLIDEPATH%\xalan\xalan.jar
 set SLIDECLASSPATH=%SLIDECLASSPATH%;%SLIDEPATH%\xalan\xercesImpl.jar
